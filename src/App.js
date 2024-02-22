8de87a4facc69f93dd07eb350639efad9c4eced9
Manoj <maanojkumar18@gmail.com>
2024-02-22 16:25:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function App() {
+  let [products,setproducts] = useState([]);
+  let getProducts = () => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => res.data)
+      .then((resFinal) => {
+        setproducts(resFinal.products);
+      })
+      .catch((error) => {
+       
+      });
+  };
+
+  useEffect(() => {
+    getProducts();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-[30px] text-center py-6 font-bold ">Our Products</h1>
+
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 max-w-[1320px] mx-auto gap-5">
+        {products.length > 1 ? (
+          products.map((items, index) => {
+           return <ProductCard items={items} key={index} />;
+          })
+        ) : (
+          <div> Loading......</div>
+        )}
+      </div>
     </div>
   );
 }
 
+function ProductCard({items}) {
+  return (
+    <div className=" shadow-lg ">
+      <Link to={`/products/${items.id}`}>
+      <img
+        className=" w-[100%] h-[230px] "
+        src={items.thumbnail}
+      />
+      <h3 className="text-center py-3 bg-slate-400 text-yellow-100 font-bold">
+        {" "}
+        {items.title}
+      </h3>
+      </Link>
+     
+
+    </div>
+  );
+}
 export default App;
---
+++ src/ProductDetails.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function ProductDetails() {
+  let userParam = useParams();
+  let productId = userParam.id;
+  let [productDetails, setProductDetails] = useState({});
+  let [productImages, setProductImages] = useState([]);
+  let [singleImage, setSingleImage] = useState("");
+
+
+  useEffect(() => {
+    axios
+      .get(`https://dummyjson.com/products/${productId}`)
+      .then((res) => res.data)
+      .then((finalRes) => {
+        setProductDetails(finalRes);
+        setProductImages(finalRes.images);
+        setSingleImage(finalRes.thumbnail);
+      });
+  }, [productId]);
+
+  return (
+    <>
+      <div className=" py-4 text-center font-bold bg-indigo-200 text-[30px]">
+        {productDetails.title}
+      </div>
+      <div className=" max-w-[1320px]  py-10 mx-auto grid lg:grid-cols-2 gap-10" >
+        <div className="">
+          {
+            setSingleImage!=="" ?
+            <img className=" w-[100%] h-[400px] " src={singleImage} />
+            :
+            ' '
+          };
+         
+          <div className=" flex gap-[10px] py-3 ">
+
+            {productImages.map((images, i) => {
+              return (
+                <div>
+                  <img className="h-[100px] basis-[25%] border border-yellow-800" src={images} onClick={()=> setSingleImage(images)} key={i} />
+                </div>
+              )
+            })
+            }
+
+          </div>
+        </div>
+        <div>
+          <h1 className=" text-3xl  font-bold  "> {productDetails.title}</h1>
+          <p> {productDetails.category} </p>
+          <h2 className=" text-xl py-1">Rs. {productDetails.price}</h2>
+          <p className=" py-3 "> {productDetails.description}</p>
+        </div>
+      </div>
+
+      <Link to={"/"} className="mx-auto table bg-[lightBlue] p-[10px]">
+        Back To Home
+      </Link>
+    </>
+  );
+}
